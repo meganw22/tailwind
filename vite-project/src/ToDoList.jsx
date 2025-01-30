5d664983221e89ae543233a71ffd5071ff6eb569
meganw22 <mwalford22@gmail.com>
2025-01-30 21:10:47 +0000

    Initial commit with Tailwindcss installed

---
+++ vite-project/src/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToDoList() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-red-600">Hello world!</h1>
+    </>
+  );
+}
+
+export default ToDoList;
