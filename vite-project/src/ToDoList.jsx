a5b105feba495df3d509fa58994dd560d316ed7f
meganw22 <mwalford22@gmail.com>
2025-02-05 21:25:56 +0000

    Function to addTask() completed

--- vite-project/src/ToDoList.jsx
+++ vite-project/src/ToDoList.jsx
@@ -8,25 +8,49 @@ function ToDoList() {
     setNewTask(event.target.value);
   }
 
-  function addTask() {}
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTask((t) => [...task, newTask]);
+      setNewTask("");
+    }
+  }
 
   function deleteTask(index) {}
 
   return (
     <>
-      <div>
-        <h1 className="to-do-title font-bold text-3xl mt-10">To-Do List</h1>
-        <div className="flex justify-center">
-          <input
-            type="text"
-            className="newTaskBox"
-            value={newTask}
-            onChange={handleInputChange}
-            placeholder="enter new task..."
-          />
-          <button>Add</button>
-          <br />
-          <ol></ol>
+      <div className="flex justify-center h-screen">
+        <div className="text-center">
+          <h1 className="to-do-title font-bold text-3xl mt-10">To-Do List</h1>
+          <div>
+            <input
+              type="text"
+              className="newTaskBox"
+              value={newTask}
+              onChange={handleInputChange}
+              placeholder="enter new task..."
+            />
+
+            <button className="add-button" onClick={addTask}>
+              add
+            </button>
+
+            <br />
+          </div>
+
+          <ol className="list-decimal">
+            {task.map((task, index) => (
+              <li key={index}>
+                <span className="text">{task}</span>
+                <button
+                  className="delete-button"
+                  onClick={() => deleteTask(index)}
+                >
+                  delete
+                </button>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     </>
