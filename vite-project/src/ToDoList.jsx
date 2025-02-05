efce2812d8166c6d379342a5bd92b723114e4675
meganw22 <mwalford22@gmail.com>
2025-02-05 20:33:46 +0000

    Set up of functions and const for To-Do List

--- vite-project/src/ToDoList.jsx
+++ vite-project/src/ToDoList.jsx
@@ -1,5 +1,28 @@
 import React, { useState } from "react";
 
-function ToDoList() {}
+const ToDoList = () => {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {}
+
+  function addTask() {}
+
+  function deleteTask(index) {}
+
+  return (
+    <>
+      <div className="to-do-title font-bold text-3xl">To-Do List</div>
+      <input
+        type="text"
+        className="newTaskBox"
+        value={newTask}
+        onChange={handleInputChange}
+        placeholder="enter new task..."
+      />
+      <button>Add</button>
+    </>
+  );
+};
 
 export default ToDoList;
